a10f861d6e4be14b72bf121b77716067a8f85371
jntakpe <jntakpe@gmail.com>
2018-10-26 14:38:42 +0200

    Add Avro support

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,11 @@
+import com.commercehub.gradle.plugin.avro.GenerateAvroJavaTask
+import com.commercehub.gradle.plugin.avro.GenerateAvroProtocolTask
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import java.net.URI
 
 plugins {
     kotlin("jvm") version "1.2.71"
+    id("com.commercehub.gradle.plugin.avro") version "0.9.1"
 }
 
 group = "com.github.jntakpe"
@@ -11,9 +14,11 @@ version = "1.0-SNAPSHOT"
 dependencies {
     compile(kotlin("stdlib-jdk8", "1.2.71"))
     compile("org.apache.kafka:kafka-clients:1.0.0-cp1")
+    compile("org.apache.kafka:kafka-streams:1.0.0-cp1")
     compile("ch.qos.logback:logback-classic:1.2.3")
     compile("org.slf4j:slf4j-api:1.7.25")
     compile("io.projectreactor.kafka:reactor-kafka:1.1.0.RELEASE")
+    compile("org.apache.avro:avro:1.8.2")
 }
 
 tasks {
@@ -21,6 +26,10 @@ tasks {
         kotlinOptions {
             jvmTarget = "1.8"
         }
+    }
+    withType<GenerateAvroProtocolTask> {
+        source("src/avro")
+        setOutputDir(file("dest/avro"))
     }
 }
 
